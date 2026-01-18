ecb86e4e1308f9bfb26115473fb406ad921dd62a
ChrisMin81 <christian.minatti@gmail.com>
2026-01-18 09:42:07 +0100

    Update `package-lock.json` to remove unused dependencies and adjust metadata.

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -4,16 +4,14 @@ import { Dashboard } from './app/pages/dashboard/dashboard';
 import { Documentation } from './app/pages/documentation/documentation';
 import { Landing } from './app/pages/landing/landing';
 import { Notfound } from './app/pages/notfound/notfound';
+import { isDevMode } from '@angular/core';
 
 export const appRoutes: Routes = [
     {
         path: '',
         component: AppLayout,
         children: [
-            { path: '', component: Dashboard },
-            { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
-            { path: 'documentation', component: Documentation },
-            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
+            ...loadDevChildren(),
         ]
     },
     { path: 'landing', component: Landing },
@@ -21,3 +19,14 @@ export const appRoutes: Routes = [
     { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
     { path: '**', redirectTo: '/notfound' }
 ];
+
+function loadDevChildren(): Routes {
+    return isDevMode()
+        ? [
+              { path: '', component: Dashboard },
+              { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
+              { path: 'documentation', component: Documentation },
+              { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
+          ]
+        : [];
+}
